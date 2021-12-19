d57f325a4411ac21f4a43181010139a5f42af933
Engincan Danabaş <engincandanabas@ogr.erbakan.edu.tr>
2021-12-19 22:44:47 +0300

    .
    .

--- Assets/Scripts/PlayerChildTrigger.cs
+++ Assets/Scripts/PlayerChildTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerChildTrigger : MonoBehaviour
 {
-    public GameObject group,holder;
+    public GameObject group, holder;
     public PlayFabManager playFabManager;
 
     public GameObject energyShield;
@@ -35,7 +35,7 @@ public class PlayerChildTrigger : MonoBehaviour
                 playerMovement.armor = false;
                 collision.gameObject.GetComponent<Bubble>().DestroyBubble();
             }
-                
+
         }
         else
         {
@@ -45,7 +45,7 @@ public class PlayerChildTrigger : MonoBehaviour
 
     private void CheckItems(Collider2D collision)
     {
-        if(collision.name == "Grenade")
+        if (collision.name == "Grenade")
         {
             Destroy(collision.gameObject);
             List<GameObject> bubbles = new List<GameObject>();
@@ -56,9 +56,10 @@ public class PlayerChildTrigger : MonoBehaviour
             foreach (GameObject bubble in bubbles)
             {
                 bubble.GetComponent<Bubble>().DestroyBubble();
-            }                      
+                //Screen Shake Vibration
+            }
         }
-        else if(collision.name == "Armor")
+        else if (collision.name == "Armor")
         {
             Destroy(collision.gameObject);
             transform.parent.GetComponent<PlayerMovement>().armor = true;
