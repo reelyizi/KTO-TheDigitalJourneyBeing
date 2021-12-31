128d0d1c9d5d3cb6f9a482b9fe3b6ed4dec6c7de
reelyizi <batuhan.eyuboglu@outlook.com>
2021-12-31 07:42:33 +0300

    Setting test

---
+++ Assets/Scripts/Settings/FillBar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class FillBar
+{
+    public string fillBarName;
+    public float fillBarAmount;
+    public GameObject baseVolume;
+    public Transform knob;
+    public GameObject area;
+}
